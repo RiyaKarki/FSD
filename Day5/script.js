e096c2c85f4d85cd77c5bd31509755e57f8f196e
RiyaKarki <karkiriya0029@gmail.com>
2024-10-15 15:44:22 +0530

    queryselector

---
+++ Day5/script.js
@@ -0,0 +1,19 @@
+// let x=confirm("Are you sure to logout");
+// alert(x)
+
+// let v=prompt("Enter your name","Riya");
+// alert(v);
+
+// let disp=()=>{
+//     console.log("Welcome to arrow funtion in js");
+// }
+// disp();
+
+// let say=()=> console.log("Single arrow fun");
+// say();
+
+// let sum=(x,y)=>{
+//     return(x+y);
+// }
+// let res=sum(13,12)
+// console.log(res)
