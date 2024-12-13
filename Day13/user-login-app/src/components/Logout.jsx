e65dfde659e14615847df9b3dc50a6533e294d00
RiyaKarki <karkiriya0029@gmail.com>
2024-12-13 10:02:19 +0530

    logout

--- Day13/user-login-app/src/components/Logout.jsx
+++ Day13/user-login-app/src/components/Logout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {useNavigate} from "react-router-dom";
 
-const Logout = () => {
+const Logout = ({regLog}) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    regLog(null);
+    navigate("/");
+  }, []);
   return (
     <div>
-      
+      Logout
     </div>
   )
 }
